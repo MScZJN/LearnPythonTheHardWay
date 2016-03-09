24176817f0a0fa7228cc1f36dafb9fa0d3cb904c
MScZJN <msczjn@gmail.com>
2016-03-09 15:12:08 +0800

    Update to ex23.

---
+++ ex5.py
@@ -0,0 +1,17 @@
+#-- coding: utf-8 --
+
+my_name = 'Jianan ZHU'
+my_age = 25 # not a lie
+my_height = 175
+my_weight = 144
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s" % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
